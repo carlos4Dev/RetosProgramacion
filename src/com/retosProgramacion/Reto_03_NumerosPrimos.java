44882749a15bdd3bbae1f2ab0dd857a7ecb59e48
Carlos Parra <74877027+carlos4Dev@users.noreply.github.com>
2023-01-28 07:52:24 +0100

    Añado Reto_03_NumerosPrimos.java desde MacBook

---
+++ src/com/retosProgramacion/Reto_03_NumerosPrimos.java
@@ -0,0 +1,28 @@
+package com.retosProgramacion;
+
+public class Reto_03_NumerosPrimos {
+
+    /*
+     * Escribe un programa que se encargue de comprobar si un número es o no primo.
+     * Hecho esto, imprime los números primos entre 1 y 100.
+     */
+
+    public static void main (String args[]) {
+        int limite = 100;
+        for (int i = 0; i <= limite; i++) {
+            if (isPrimo(i)) {
+                System.out.println("El número " + i + " es primo");
+            }
+        }
+
+    }
+
+    private static boolean isPrimo(int num) {
+        for (int n = 2; n < num; n++) {
+            if (num % n == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
