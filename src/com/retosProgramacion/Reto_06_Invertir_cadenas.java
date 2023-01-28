c074e2cda12480a2eb24e293fca53a9180bd8705
Carlos Parra <74877027+carlos4Dev@users.noreply.github.com>
2023-01-28 18:42:21 +0100

    Añado Reto_06_Invertir_cadenas

---
+++ src/com/retosProgramacion/Reto_06_Invertir_cadenas.java
@@ -0,0 +1,36 @@
+package com.retosProgramacion;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class Reto_06_Invertir_cadenas {
+
+    /*
+     * Crea un programa que invierta el orden de una cadena de texto
+     * sin usar funciones propias del lenguaje que lo hagan de forma automática.
+     * - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+     */
+
+    public static void main (String args[]) {
+
+        String name = "Hola Mundo";
+        ArrayList<Character> alreves = new ArrayList<Character>();
+        int lastPosition = name.length() - 1;
+
+        for (int i = lastPosition; i >= 0; i--){
+            alreves.add(name.charAt(i));
+        }
+        /*
+        for(int i = 0; i < alreves.size(); i++) {
+            System.out.print(alreves.get(i));
+        }
+        */
+        String str = alreves.stream()
+                .map(e->e.toString())
+                .collect(Collectors.joining());
+
+        System.out.println(str);
+
+    }
+
+}
