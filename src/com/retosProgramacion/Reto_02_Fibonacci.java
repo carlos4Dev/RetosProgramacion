5a3250af53907c759bd5a769dea41a163f0307f8
Carlos Parra <bitberri@gmail.com>
2023-01-28 06:43:44 +0100

    Añado Reto_02_Fibonacci.java desde portátil HP

---
+++ src/com/retosProgramacion/Reto_02_Fibonacci.java
@@ -0,0 +1,29 @@
+package com.retosProgramacion;
+
+public class Reto_02_Fibonacci {
+
+    /*
+     * Escribe un programa que imprima los 50 primeros números de la sucesión
+     * de Fibonacci empezando en 0.
+     * - La serie Fibonacci se compone por una sucesión de números en
+     *   la que el siguiente siempre es la suma de los dos anteriores.
+     *   0, 1, 1, 2, 3, 5, 8, 13...
+     */
+
+    public static void main (String args[]) {
+
+        long num1 = 0, num2 = 1, suma = 1, serie = 50;
+
+        // Imprimir el primer número
+        System.out.println(num1);
+
+        for (int i = 0; i < serie; i++) {
+            // Imprimir valor actual de suma
+            System.out.println(suma);
+            suma = num1 + num2;
+            num1 = num2;
+            num2 = suma;
+        }
+
+    }
+}
