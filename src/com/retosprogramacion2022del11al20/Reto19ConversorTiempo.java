e9a90419e12eceebd4140663b519e6a27b0429ae
Carlos Parra <74877027+carlos4Dev@users.noreply.github.com>
2023-03-03 20:53:43 +0100

    Añado Reto 19 Conversor Tiempo

---
+++ src/com/retosprogramacion2022del11al20/Reto19ConversorTiempo.java
@@ -0,0 +1,13 @@
+package com.retosprogramacion2022del11al20;
+
+public class Reto19ConversorTiempo {
+
+    /*
+     * Crea una función que reciba días, horas, minutos y segundos (como enteros)
+     * y retorne su resultado en milisegundos.
+     */
+
+    public static void main (String[] args) {
+        
+    }
+}
